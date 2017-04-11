6558f6ce71de22bb935ce8a3055e144415ecf07c
Petri Salminen <petri.salminen@student.tut.fi>
2017-04-11 15:15:48 +0300

    =Mainin operaatioita siirretty apufunktioihin

--- ElectionGame/Student/main.cpp
+++ ElectionGame/Student/main.cpp
@@ -22,6 +22,9 @@ using Interface::Player;
 using std::make_shared;
 using std::shared_ptr;
 
+
+void addlocations(shared_ptr<Game> game);
+void addcards(shared_ptr<Game> game);
 /*
  * The main program should initialize the game and open the main window, or
  * delegate these tasks elsewhere.
@@ -38,31 +41,11 @@ int main(int argc, char* argv[])
     // set up locations of the board
     {
         // create and initialize a location, and add it to the game
-        shared_ptr<Location> location1 = make_shared<Location>(1, "Metsämiesten kilta");
-        shared_ptr<Location> location2 = make_shared<Location>(2, "Hengenpelastajatytöt");
-        shared_ptr<Location> location3 = make_shared<Location>(3, "Kaupungin kuumakulmakundit");
-        shared_ptr<Location> location4 = make_shared<Location>(4, "Kallion huolestuneet kansalaiset");
-        location1->initialize();
-        game->addLocation(location1);
-        location2->initialize();
-        game->addLocation(location2);
-        location3->initialize();
-        game->addLocation(location3);
-        location4->initialize();
-        game->addLocation(location4);
+        addlocations(game);
 
         // set up cards for the location deck
         {
-            // create an influence card and add it to location deck
-            shared_ptr<Influence> influence1 = make_shared<Influence>(location1->name() + " Influence", location1, 1);
-            location1->deck()->addCard(influence1);
-
-            // TODO: create more cards
-            shared_ptr<Agent> agentti = make_shared<Agent>("koeakentti",true);
-            location1->deck()->addCard(agentti);
-
-            // shuffle the deck
-            location1->deck()->shuffle();
+            addcards(game);
         }
 
         // TODO: create more locations
@@ -82,4 +65,34 @@ int main(int argc, char* argv[])
     // open the main window
     //window.addPlayer(playervector.at(0));
     a.exec();
+}
+
+void addlocations(shared_ptr<Game> game){
+    shared_ptr<Location> location1 = make_shared<Location>(1, "Metsämiesten kilta");
+    shared_ptr<Location> location2 = make_shared<Location>(2, "Hengenpelastajatytöt");
+    shared_ptr<Location> location3 = make_shared<Location>(3, "Kaupungin kuumakulmakundit");
+    shared_ptr<Location> location4 = make_shared<Location>(4, "Kallion huolestuneet kansalaiset");
+    location1->initialize();
+    game->addLocation(location1);
+    location2->initialize();
+    game->addLocation(location2);
+    location3->initialize();
+    game->addLocation(location3);
+    location4->initialize();
+    game->addLocation(location4);
+};
+
+void addcards(shared_ptr<Game> game){
+    // create an influence card and add it to location deck
+    shared_ptr<Location> location1 = game->locations().at(0);
+    shared_ptr<Influence> influence1 = make_shared<Influence>(location1->name() + " Influence", location1, 1);
+    location1->deck()->addCard(influence1);
+    //game->locations().at(0)->addCard(influence1)
+
+    // TODO: create more cards
+    shared_ptr<Agent> agentti = make_shared<Agent>("koeakentti",true);
+    location1->deck()->addCard(agentti);
+
+    // shuffle the deck
+    location1->deck()->shuffle();
 }
