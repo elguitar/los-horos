ca33965d90780f1298006eaa2cf1ae3e05e2d486
Ilpo Toikkanen <toikkani@linux-desktop5.cc.tut.fi>
2017-04-11 20:02:58 +0300

    =action nosta agenttia tehty, valittaa vielä pointtereiden vertailusta

--- ElectionGame/Student/actionnostaagentti.cpp
+++ ElectionGame/Student/actionnostaagentti.cpp
@@ -18,10 +18,30 @@ ActionNostaAgentti::~ActionNostaAgentti()
 
 bool ActionNostaAgentti::canPerform() const
 {
-
+    if (peli_->currentPlayer()->cards().size() < 7)
+    {
+        return true;
+    }
+    else
+    {
+       return false;
+    }
 }
 
 void ActionNostaAgentti::perform()
 {
+    //for (std::shared_ptr<Agent> agentti : location_->agents())
+    for (uint i = 0; i < location_->agents().size(); ++i)
+    {
+        //std::set<std::shared_ptr<Interface::AgentInterface> >::iterator = location_->agents().begin();
+        std::shared_ptr<Interface::AgentInterface> agentti = *std::next(location_->agents().begin(), i);
 
+        /*täs "no match for operator==" weak_ptr vs. shared_ptr
+         *
+         * if (agentti->owner() == peli_->currentPlayer())
+        {
+            peli_->currentPlayer()->addCard(agentti);
+            location_->removeAgent(agentti);
+        }*/
+    }
 }
